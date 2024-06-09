d2e2e3fa00b59bca1abe39cfed870558ec04ea5c
Shreyas Sahoo <shreyassahoo26@gmail.com>
2024-06-09 16:58:06 +0530

    Initial Commit

---
+++ RLS-Buddy-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RLS-Buddy-Assignment
+//
+//  Created by Shreyas Sahoo on 09/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
